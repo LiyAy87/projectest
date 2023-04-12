0580541e95e2165a5ed4d4d2cf30377115c71839
Patricia Colas Barriocanal <pcolas@hiberus.com>
2023-04-12 17:24:13 +0200

    actualizando

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "projectest": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
